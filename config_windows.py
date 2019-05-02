ae6fe8e6fd24029e73912d8c9192072a5269732f
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-05-02 09:43:25 +0200

    windows config

---
+++ config_windows.py
@@ -0,0 +1,64 @@
+# Customize your buttons here
+# -----------------------
+# Windows settings
+
+right = 'RIGHT_ARROW'
+left = 'LEFT_ARROW'
+up = 'UP_ARROW'
+down = 'DOWN_ARROW'
+start = "BTN_SELECT"
+select = "BTN_START"
+y = "BTN_NORTH"
+x = "BTN_WEST"
+a = "BTN_SOUTH"
+b = "BTN_EAST"
+trigger_power = 150
+
+# -----------------------
+
+# System configuration
+
+left_ctrl = 'left ctrl'
+
+f1 = 'f1'
+f2 = 'f2'
+f3 = 'f3'
+f4 = 'f4'
+f5 = 'f5'
+f6 = 'f6'
+f7 = 'f7'
+f8 = 'f8'
+f9 = 'f9'
+
+cf1 = 'ctrl+f1'
+cf2 = 'ctrl+f2'
+cf3 = 'ctrl+f3'
+cf4 = 'ctrl+f4'
+cf5 = 'ctrl+f5'
+cf6 = 'ctrl+f6'
+cf7 = 'ctrl+f7'
+cf8 = 'ctrl+f8'
+
+# -----------------------
+# Do not change unless you know what you are doing.
+# They are the same on Linux and Windows !
+# -----------------------
+
+# numpad
+numpad_1 = 79
+numpad_3 = 81
+numpad_7 = 71
+numpad_9 = 73
+
+# d-pad
+d_pad_x_axis = "ABS_HAT0X"
+d_pad_y_axis = "ABS_HAT0Y"
+
+# bumpers
+lb = "BTN_TL"
+rb = "BTN_TR"
+
+# triggers
+lt = "ABS_Z"
+rt = "ABS_RZ"
+# -----------------------
