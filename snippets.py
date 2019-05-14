b908f0fb3a795b4f7cee991851a53d1cc51370f7
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-05-14 12:55:20 +0200

    snippet for holding lb + button

--- snippets.py
+++ snippets.py
@@ -4,7 +4,6 @@ All you have to do is configure, copy and paste it in [controller_name].py file
 in which preset best suits your needs.
 Feel free to implement your own in [controller_name].py
 """
-
 """
 Right analog - beta
 """
@@ -27,3 +26,14 @@ if event.code == config.left_stick_y_axis and 20000 < event.state:
     pyautogui.hotkey('shift', 'f7')
 if event.code == config.left_stick_y_axis and event.state < -25000:
     pyautogui.hotkey('shift', 'f8')
+"""
+holding lb with buttons to do 'ctrl+shift+f*' hotkey
+you have to change existing button in [controller_name].py in desired preset
+(state 1 is for pressed and 0 for release)
+"""
+if event.code == config.lb and event.state == 1:
+    pyautogui.keyDown('ctrl')
+    pyautogui.keyDown('shift')
+if event.code == config.lb and event.state == 0:
+    pyautogui.keyUp('ctrl')
+    pyautogui.keyUp('shift')
